3274a09ac28a15e4ae390f915b862aedeb6fea81
Celine Patag Miranda <celine.patag@gmail.com>
2017-05-15 10:41:17 +0800

    eslint setup

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,4 @@
-/* eslint-disable */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { AppRegistry } from 'react-native'
+import MedicineTracker from './src/index'
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Text,
-  View,
-} from 'react-native';
-
-export default class MedicineTracker extends Component {
-  render() {
-    return (
-      <View>
-        <Text>
-          Welcome to React Native!
-        </Text>
-      </View>
-    );
-  }
-}
-
-AppRegistry.registerComponent('MedicineTracker', () => MedicineTracker);
+AppRegistry.registerComponent('MedicineTracker', () => MedicineTracker)
---
+++ src/index.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react'
+import {
+  Text,
+  View,
+} from 'react-native'
+
+const MedicineTracker = () => (
+  <View>
+    <Text>
+        Welcome to React Native!
+      </Text>
+    <Text>
+        Welcome to React Native!
+      </Text>
+    <Text>
+        Welcome to React Native!
+      </Text>
+    <Text>
+        Welcome to React Native!
+      </Text>
+  </View>
+  )
+
+export default MedicineTracker
